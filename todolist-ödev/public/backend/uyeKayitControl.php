2b9e1ce93f4fbaaf0224a6d8bd57de9637f26031
Ramazanyergun <yergunr@gmail.com>
2023-07-28 14:26:15 +0300

    bir önceki ile aynı

--- todolist-ödev/public/backend/uyeKayitControl.php
+++ todolist-ödev/public/backend/uyeKayitControl.php
@@ -7,16 +7,14 @@ $surname = $_POST["surname"];
 $email = $_POST["email"];
 $password = $_POST["password"];
 
-$userInsert  = "insert into user (name,surname,email,password) values (?,?,?,?)";
+$userInsert  = "insert into user (name ,surname, email, password) values (?,?,?,?)";
 $query = $connection->prepare($userInsert);
 $result = $query->execute([$name, $surname, $email, $password]);
 
-if ($userInsert) {
+if ($result) {
     header("Location : http://localhost/Ödev/todolist-ödev/public/frontend/uyeGiris.php?state=1");
     exit();
 } else {
     header("Location : http://localhost/Ödev/todolist-ödev/public/frontend/uyeKayit.php?state=0");
     exit();
 }
-
-?>
